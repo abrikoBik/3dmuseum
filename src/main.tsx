8a9735adaeae6e7f7bf95e8ed2251092a2e0ae0f
Daniil Popov <abereje@localhost.localdomain>
2025-06-03 13:00:01 +0300

    ADD ALL NEW FILES

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import './index.css'
+import Space from './Space.tsx'
+import Spy from './Spy.tsx'
+import Home from './Home.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/spy" element={<Spy/>}/>
+        <Route path="/space" element={<Space/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
+  </StrictMode>,
+)
